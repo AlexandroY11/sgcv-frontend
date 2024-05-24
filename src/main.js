4b7133d9e717a6cea8eb754d23ea86b45a329294
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-24 18:41:26 -0500

    Importando librerias

--- src/main.js
+++ src/main.js
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBank, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
+library.add(faPhone, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser, faBank);
+
+createApp(App).use(router)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
